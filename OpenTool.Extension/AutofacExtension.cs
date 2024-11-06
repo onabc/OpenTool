6ef582a99e7585c879b3cfdeeadb9d465112379a
70KG <hesbiu@gmail.com>
2024-11-06 15:29:44 +0800

    feat

---
+++ OpenTool.Extension/AutofacExtension.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Builder;
+using Autofac.Features.Scanning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace OpenTool.Extension
+{
+    public static class AutofacExtension
+    {
+        /// <summary>
+        /// 用于辅助批量注册，筛选继承于指定类型的子类。
+        /// </summary>
+        /// <typeparam name="T">指定类型的基类，所有继承与它的子类都会被包含</typeparam>
+        public static IRegistrationBuilder<object, ScanningActivatorData, DynamicRegistrationStyle> RegisterAssignableAssemblyTypes<T>(this ContainerBuilder builder, IEnumerable<Assembly> assemblies)
+        {
+            if (assemblies is null)
+            {
+                throw new ArgumentNullException(nameof(assemblies));
+            }
+
+            return builder.RegisterAssemblyTypes(assemblies.ToArray())
+                .AssignableTo<T>();
+        }
+    }
+}
---
+++ OpenTool.Windows/SingleInstanceApp/SingleInstanceApp.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace OpenTool.Windows
+{
+    public static class SingleInstanceApp
+    {
+        private static SingleInstance singleInstance;
+
+        static SingleInstanceApp()
+        {
+            Configure();
+        }
+
+        private static void Configure()
+        {
+            string appTypeName = Application.Current.GetType().FullName;
+            string identifier = @$"SingleInstance_{appTypeName}_PassArgumentsAsync";
+            singleInstance = new SingleInstance(identifier);
+
+            singleInstance.ArgumentsReceived.Subscribe(args =>
+            {
+                SingleInstance.ActivateWindow(Process.GetCurrentProcess());
+            });
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public static bool InitializeAsFirstInstance(object sender, StartupEventArgs e)
+        {
+            bool isFirstInstance = singleInstance.IsFirstInstance;
+            if (!isFirstInstance)
+            {
+                singleInstance.PassArgumentsToFirstInstance(e.Args);
+                if (sender is Application app) app.Shutdown();
+            }
+            else
+            {
+                singleInstance.ListenForArgumentsFromSuccessiveInstances();
+            }
+            return isFirstInstance;
+        }
+    }
+}
