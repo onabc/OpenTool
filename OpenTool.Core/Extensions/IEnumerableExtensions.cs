03cdce257e10be9c355d1f77206797a6e9b43a27
70KG <hesbiu@gmail.com>
2025-06-16 10:50:04 +0800

    feat(*): 移除windows模块

---
+++ OpenTool.Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenTool.Extension
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Select using the given action, but ignore exceptions and skip offending items.
+        /// </summary>
+        public static IEnumerable<TOut> Attempt<TIn, TOut>(this IEnumerable<TIn> baseEnumerable,
+            Func<TIn, TOut> action)
+        {
+            foreach (var item in baseEnumerable)
+            {
+                TOut output;
+                try
+                {
+                    output = action(item);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Attempt failed, skipping. Error: " + e);
+                    continue;
+                }
+                yield return output;
+            }
+        }
+    }
+}
