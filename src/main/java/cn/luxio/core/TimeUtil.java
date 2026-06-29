565b5f68ff0d81a39253590c964111ab96042146
luxiong <luxiong@iotpsvr.cn>
2026-06-29 16:07:27 +0800

    first commit

---
+++ src/main/java/cn/luxio/core/TimeUtil.java
@@ -0,0 +1,12 @@
+package cn.luxio.core;
+
+public class TimeUtil {
+
+    /**
+     * 获取当前时间戳
+     * @return 时间戳
+     */
+    public static long getCurrentTime() {
+        return System.currentTimeMillis();
+    }
+}
